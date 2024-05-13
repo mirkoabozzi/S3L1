06ddcf4e90401275873d85adf240f82b20a2fa55
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-05-13 17:21:24 +0200

    update

--- D7.js
+++ D7.js
@@ -3,14 +3,14 @@
   prima e gli ultimi 3 della seconda. Converti la stringa risultante in maiuscolo e mostrala con un console.log().
 */
 
-// function es1(string1, string2) {
-//   let str1 = string1.substring(0, 2);
-//   let str2 = string2.slice(-3);
-//   let unite = str1 + str2;
-//   unite.touppercase();
-//   return unite;
-// }
-// result = es1("Mirko", "Abozzi");
+function es1(string1, string2) {
+  let str1 = string1.substring(0, 2);
+  let str2 = string2.slice(-3);
+  let unite = str1 + str2;
+  unite.toUpperCase();
+  return unite;
+}
+console.log(es1("Mirko", "Abozzi"));
 
 /* ESERCIZIO 2 (for)
   Scrivi una funzione che torni un array di 10 elementi; ognuno di essi deve essere un valore random compreso tra 0 e 100 (incluso).
